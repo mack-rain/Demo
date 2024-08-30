2f6812f91aa12de5557fa2ad0b629665bccdd66d
mack-rain <165780301+mack-rain@users.noreply.github.com>
2024-08-30 16:38:44 +0800

    Update CustomerController.cs

--- CustomerDemo/Controllers/CustomerController.cs
+++ CustomerDemo/Controllers/CustomerController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,121 +13,129 @@ namespace CustomerDemo.Controllers
         private static Dictionary<long, CustomerModel> customDic = new Dictionary<long, CustomerModel>();
 
         [HttpGet("leaderboard")]
-        public IActionResult LeaderBoard([FromQuery] int start, [FromQuery] int end)
+        public async Task<IActionResult> LeaderBoard([FromQuery] int start, [FromQuery] int end)
         {
-            List<CustomerModel> result = new List<CustomerModel>();
-            int index = 1;
-            customDic.Values.ToList().OrderByDescending(a => a.Score).ThenBy(a => a.CustomerID).ToList().ForEach(item => {
-
-                item.Rank = index;
-
-                if (index >= start&&index<= end)
-                {
-                    result.Add(item);
-                }
-
-                if (index > end)
-                {
-                    return;
-                }
-
-                index++;
-
-
-            });
-
-            return RenderView(result);
-        }
-
-        [HttpGet("leaderboard/{customerid:long}")]
-        public IActionResult GetCustomersByID(long customerId, [FromQuery] int high, [FromQuery] int low)
-        {
-            List<CustomerModel> result = new List<CustomerModel>();
-            bool isFind = false;
-            int index = 1;
-            Dictionary<int, CustomerModel> keyValuePairs = new Dictionary<int, CustomerModel>();
-            if (customDic.ContainsKey(customerId))
+            return await Task.Run(() =>
             {
-                customDic.Values.ToList().OrderByDescending(a => a.Score).ThenBy(a => a.CustomerID).ToList().ForEach(item => {
+                List<CustomerModel> result = new List<CustomerModel>();
+                int index = 1;
+                customDic.Values.ToList().OrderByDescending(a => a.Score).ThenBy(a => a.CustomerID).ToList().ForEach(item =>
+                {
                     item.Rank = index;
-                    if (!isFind)
+
+                    if (index >= start && index <= end)
                     {
-                        if (item.CustomerID != customerId)
-                        {
-                            if (keyValuePairs.Keys.Contains(index % high))
-                            {
-                                keyValuePairs[index % high] = item;
-                            }
-                            else
-                            {
-                                keyValuePairs.Add(index % high, item);
-                            }
-                        }
-                        else
-                        {
-                            isFind = true;
-                            foreach (KeyValuePair<int, CustomerModel> keyValuePair in keyValuePairs)
-                            {
-                                result.Add(keyValuePair.Value);
-                            }
-                            result.Add(item);
-                        }
+                        result.Add(item);
                     }
-                    else
+
+                    if (index > end)
                     {
-                        if (low > 0)
-                        {
-                            low--;
-                            result.Add(item);
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        return;
                     }
-                    
 
                     index++;
                 });
-            }
 
-            return RenderView(result);
+                return RenderView(result);
+            });
+        }
+
+        [HttpGet("leaderboard/{customerid:long}")]
+        public async Task<IActionResult> GetCustomersByID(long customerId, [FromQuery] int high, [FromQuery] int low)
+        {
+            return await Task.Run(() =>
+            {
+                List<CustomerModel> result = new List<CustomerModel>();
+                bool isFind = false;
+                int index = 1;
+                Dictionary<int, CustomerModel> keyValuePairs = new Dictionary<int, CustomerModel>();
+
+                if (customDic.ContainsKey(customerId))
+                {
+                    customDic.Values.ToList().OrderByDescending(a => a.Score).ThenBy(a => a.CustomerID).ToList().ForEach(item =>
+                    {
+                        item.Rank = index;
+                        if (!isFind)
+                        {
+                            if (item.CustomerID != customerId)
+                            {
+                                if (keyValuePairs.Keys.Contains(index % high))
+                                {
+                                    keyValuePairs[index % high] = item;
+                                }
+                                else
+                                {
+                                    keyValuePairs.Add(index % high, item);
+                                }
+                            }
+                            else
+                            {
+                                isFind = true;
+                                foreach (KeyValuePair<int, CustomerModel> keyValuePair in keyValuePairs)
+                                {
+                                    result.Add(keyValuePair.Value);
+                                }
+                                result.Add(item);
+                            }
+                        }
+                        else
+                        {
+                            if (low > 0)
+                            {
+                                low--;
+                                result.Add(item);
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+
+                        index++;
+                    });
+                }
+
+                return RenderView(result);
+            });
         }
 
         [HttpPost("{customerId:long}/score/{score:decimal}")]
-        public IActionResult SetCustomer(long customerId, decimal score)
+        public async Task<IActionResult> SetCustomer(long customerId, decimal score)
         {
-            if (customerId > 0)
+            return await Task.Run(() =>
             {
-                CustomerModel temp;
-                if (!customDic.Keys.Contains(customerId))
+                if (customerId > 0)
                 {
-                    temp = new CustomerModel();
-                    temp.CustomerID = customerId;
-                    temp.Score = score;
-                    customDic.Add(customerId,temp);
-                }
-                else
-                {
-                    temp = customDic[customerId];
-                    temp.Score += score;
+                    CustomerModel temp;
+                    if (!customDic.Keys.Contains(customerId))
+                    {
+                        temp = new CustomerModel();
+                        temp.CustomerID = customerId;
+                        temp.Score = score;
+                        customDic.Add(customerId, temp);
+                    }
+                    else
+                    {
+                        temp = customDic[customerId];
+                        temp.Score += score;
+                    }
                 }
 
+                List<CustomerModel> tempList = customDic.Values.ToList();
+                int index = 1;
+                tempList.ForEach(item =>
+                {
+                    item.Rank = index;
+                    index++;
+                });
 
-            }
-            List<CustomerModel> tempList = customDic.Values.ToList();
-            int index = 1;
-            tempList.ForEach(item => {
-                item.Rank = index;
-                index++;
+                return RenderView(tempList);
             });
-            return RenderView(tempList);
         }
 
         private IActionResult RenderView(List<CustomerModel> result)
         {
             var viewModel = result.OrderBy(a => a.Rank).ToList();
-            
             return View("Index", viewModel);
         }
     }
